4755fedd310c34009c56d818bdd57447167f78bc
Iliyas Sayyad <96952923+printilu22@users.noreply.github.com>
2025-01-30 23:16:19 +0530

    Update 45. Jump Game II.java

--- LeetCode/Top Interview 150/45. Jump Game II.java
+++ LeetCode/Top Interview 150/45. Jump Game II.java
@@ -1,24 +1,36 @@
 class Solution {
     public int jump(int[] nums) {
+        
         int n = nums.length;
         if(n == 1){
             return 0;
         }
+        int jump=0, l=0, r=0;
 
-        int maxReach = 0, edge = 0, count = 0;
-        
-        for (int i = 0; i < n - 1; i++) {
-            maxReach = Math.max(maxReach, i + nums[i]);  
-            
-            if (i == edge) {  // We need to jump
-                count++;
-                edge = maxReach;  
-                
-                if (edge >= n - 1) break;  // If we can reach the last index, stop early
+        while(r < n-1){
+            int farthest = 0;
+            for(int i = l ; i<=r; i++){
+                farthest = Math.max(farthest, i + nums[i]);
             }
+            l=r+1;
+            r = farthest;
+            jump++;
         }
+        return jump;
+        // int maxReach = 0, edge = 0, count = 0;
         
-        return count;
+        // for (int i = 0; i < n - 1; i++) {
+        //     maxReach = Math.max(maxReach, i + nums[i]);  
+            
+        //     if (i == edge) {  // We need to jump
+        //         count++;
+        //         edge = maxReach;  
+                
+        //         if (edge >= n - 1) break;  // If we can reach the last index, stop early
+        //     }
+        // }
+        
+        // return count;
         // int maxjump = 0;
         // int count = 0;
         // int prevmax = 0;
