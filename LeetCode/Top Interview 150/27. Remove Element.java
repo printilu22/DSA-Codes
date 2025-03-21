e489ebf35bcf2070bdde2eedd5a22e60b12620a5
Iliyas Sayyad <96952923+printilu22@users.noreply.github.com>
2025-03-21 23:49:46 +0530

    Update 27. Remove Element.java

--- LeetCode/Top Interview 150/27. Remove Element.java
+++ LeetCode/Top Interview 150/27. Remove Element.java
@@ -10,3 +10,25 @@ class Solution {
         return index;
     }
 }
+
+
+// class Solution {
+//     public int removeElement(int[] nums, int val) {
+//         int n = nums.length;
+//         int[] temp = new int[n];  // New array to store valid elements
+//         int j = 0;  
+
+//         for (int i = 0; i < n; i++) {
+//             if (nums[i] != val) {
+//                 temp[j++] = nums[i];  // Store valid elements
+//             }
+//         }
+
+//         // Copy back the valid elements to the original array
+//         for (int i = 0; i < j; i++) {
+//             nums[i] = temp[i];
+//         }
+
+//         return j;  // Length of valid elements
+//     }
+// }
